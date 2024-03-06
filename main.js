eee1ce203660c8d4a78603ba2dde31110bc1fec7
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-06 23:13:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var famous_person = "Quaid-e-Azam";
+var message = (famous_person + "Quaid-e-Azam is the founder of Pakistan.He was born on 25th of December 1876.He worked hard for the establishment of Pakistan.He is known as the 'Father of the Nation'");
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let famous_person:string = "Quaid-e-Azam";
+let message:string = (famous_person + "Quaid-e-Azam is the founder of Pakistan.He was born on 25th of December 1876.He worked hard for the establishment of Pakistan.He is known as the 'Father of the Nation'");
+console.log(message);
